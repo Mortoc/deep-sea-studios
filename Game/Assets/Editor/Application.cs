5a07da56e0514f4fc7065e4d44f23078359b6ff2
mbittarelli <mbittarelli@splashdowngames.com>
2014-05-31 13:35:19 -0400

    - adding Application.cs to lead the project through the unity menu - the main unity entry point is in Game.cs - deleting the Main.unity scene stuff. this will be created dynamically

---
+++ Game/Assets/Editor/Application.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+public class ApplicationMenu : EditorWindow
+{
+    [MenuItem("Application/LoadProject")]
+    private static void LoadProject()
+    {
+        string unitySceneFileName = UnityEngine.Application.dataPath + "Game.unity";
+        //string unitySceneSettingsFileName = mScenePath + projectName + ".xml";
+
+        //check if we have the scene previously saved
+        if (!File.Exists(unitySceneFileName))
+        {
+            //if not, create the scene
+            EditorApplication.NewScene();
+
+            //add the weird beard scene
+            GameObject mainGameObject = new GameObject("Game");
+            mainGameObject.AddComponent<Game>();
+
+            //save the weird beard scene
+            EditorApplication.SaveScene(unitySceneFileName);
+
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+        }
+        else
+        {
+            //if yes, open the scene
+            EditorApplication.OpenScene(unitySceneFileName);
+        }
+    }
+}
---
+++ Game/Assets/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Game : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("game awake");
+    }
+}
