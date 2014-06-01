66e379aff8be46bd3417318efe97f9f897e0599a
mbittarelli <mbittarelli@splashdowngames.com>
2014-06-01 18:48:29 -0400

    - initial ambient enemy movement.  not the greatest but it's better than nothing. - spawning 5 enemies to wander around the scene.  they have very primitive local avoidance as default behavior - deleting the EnemyComponent.cs class

--- Game/Assets/Actors/ActorManager.cs
+++ Game/Assets/Actors/ActorManager.cs
@@ -10,6 +10,6 @@ public class ActorManager
     public ActorManager()
     {
         mAvatar = new Avatar(Vector3.one);
-        mEnemyManager = new EnemyManager(1);
+        mEnemyManager = new EnemyManager(5);
     }
 }
--- Game/Assets/Actors/Enemy.cs
+++ Game/Assets/Actors/Enemy.cs
@@ -7,11 +7,8 @@ public class Enemy : ActorBase
     private static int mEnemyIdCounter = 1;
     private int mId = 0;
 
-    private float mMovementSpeed = 2.0f;
-    private CharacterController mCharacterController = null;
-    private EnemyComponent mEnemyComponent = null;
-
-    private GameObject mSteeringGameObject = null;
+    private float mMovementSpeed = 0.5f;
+    private float mTurnSpeed = 2.5f;
 
     public Enemy(Vector3 initialPosition)
         : base(initialPosition)
@@ -22,35 +19,20 @@ public class Enemy : ActorBase
     {
         mGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         mGameObject.transform.position = initialPosition;
-        mGameObject.transform.localScale = new Vector3(50.0f, 50.0f, 100.0f);
+        mGameObject.transform.localScale = new Vector3(50.0f, 50.0f, 50.0f);
         mId = mEnemyIdCounter++;
         mGameObject.name = "enemy " + mId;
 
         Rigidbody rigidbody = mGameObject.AddComponent<Rigidbody>();
         rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotationZ;
+        rigidbody.detectCollisions = false;
 
-        mCharacterController = mGameObject.AddComponent<CharacterController>();
+        mGameObject.rigidbody.velocity = mGameObject.transform.forward;
 
-
-        mSteeringGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        Rigidbody steeringRigidbody = mSteeringGameObject.AddComponent<Rigidbody>();
-        steeringRigidbody.useGravity = false;
-        steeringRigidbody.isKinematic = true;
-        mSteeringGameObject.GetComponent<BoxCollider>().isTrigger = true;
-        mEnemyComponent = mSteeringGameObject.AddComponent<EnemyComponent>();
-        //GameObject.DestroyImmediate(mSteeringGameObject.GetComponent<MeshRenderer>());
-        mSteeringGameObject.name = "steering object";
-        mSteeringGameObject.transform.position = new Vector3(mGameObject.transform.position.x,
-                                                                mGameObject.transform.position.y,
-                                                                mGameObject.transform.position.z + mGameObject.transform.localScale.z * 0.5f);
-
-        mSteeringGameObject.transform.parent = mGameObject.transform;
-
-        mSteeringGameObject.transform.localScale = new Vector3(1.0f, 1.0f, 2.0f);
-        mSteeringGameObject.transform.localPosition = new Vector3(mSteeringGameObject.transform.localPosition.x,
-                                                                    mSteeringGameObject.transform.localPosition.y,
-                                                                    mSteeringGameObject.transform.localPosition.z + (mSteeringGameObject.transform.localScale.z * 0.5f));
-
+        mGameObject.AddComponent<CharacterController>();
+        
+        mMoveVector = mGameObject.transform.forward.normalized * mMovementSpeed;
     }
 
     private bool mUpdateRandomDirection = true;
@@ -58,85 +40,122 @@ public class Enemy : ActorBase
     private float mWanderInDirectionSeconds = 0.0f;
     private DateTime mPickedNewWanderDirectionTime = DateTime.Now;
 
+    private enum CollisionState
+    {
+        NoCollision,
+        CurrentlyColliding,
+        NewCollision
+    }
+
+    private CollisionState mPreviousCollisionState = CollisionState.NoCollision;
+    private CollisionState mCurrentCollisionState = CollisionState.NoCollision;
+    Vector3 mInverseDirection = Vector3.zero;
+    Vector3 mInitialDirection = Vector3.zero;
+    Vector3 mLateralForceDirection = Vector3.zero;
+    Vector3 mMoveVector = Vector3.zero;
+
     protected override void ActorUpdate()
     {
-
-
-        Vector3 moveVector = Vector3.zero;
-
         //if the enemy is colliding with something, update the forward vector away from the collision until it's no longer colliding
-        //if (mEnemyComponent.Triggers.Count > 0)
-        //{
+
+        Debug.DrawRay(  mGameObject.transform.position,
+                        mGameObject.transform.forward * 200.0f,
+                        Color.blue);
+        Debug.DrawRay(mGameObject.transform.position,
+                        mMoveVector * 200.0f,
+                        Color.yellow);
+
+        RaycastHit[] hitInfoArray = Physics.RaycastAll(mGameObject.transform.position,
+                                                       mMoveVector.normalized,
+                                                       200.0f);
+        
         RaycastHit hitInfo = new RaycastHit();
-        bool didHit = Physics.CapsuleCast(  mSteeringGameObject.transform.position - new Vector3(0.0f, 0.0f, mSteeringGameObject.collider.bounds.size.z * 0.5f),
-                                            mSteeringGameObject.transform.position + new Vector3(0.0f, 0.0f, mSteeringGameObject.collider.bounds.size.z * 0.5f),
-                                            1.0f, mGameObject.transform.forward, out hitInfo, mSteeringGameObject.collider.bounds.size.z);
-
-
-
-        Debug.DrawLine(mGameObject.transform.localPosition,
-                       mGameObject.transform.localPosition + (mGameObject.transform.forward * 3.0f)); //(mSteeringGameObject.collider.bounds.size.z)));
-            
-                                    //new Vector3( mGameObject.transform.position.x,
-                                    //mGameObject.transform.position.y,
-                                    //mGameObject.transform.position.z + mGameObject.transform.localScale.z * 0.5f) + new Vector3(0.0f, 0.0f, 2.0f));
-                        
-        if (didHit)
-        {
-            Debug.Log("hit: " + hitInfo.collider.gameObject.name);
-            Vector3 hitVector = hitInfo.point - mGameObject.transform.position;
-            float angleBetweenHitPointAndForwardVector = Vector3.Angle(hitVector, mGameObject.transform.forward);
-
-            if (Vector3.Dot(hitVector, mGameObject.transform.right) > 0.0f)
+        Collider hitCollider = null;
+        if (hitInfoArray.Length > 0)
+        {           
+            for (int i = 0; i < hitInfoArray.Length; ++i)
             {
-                mGameObject.transform.Rotate(Vector3.Cross(mGameObject.transform.forward, mGameObject.transform.right), -1.0f);
-            }
-            else
-            {
-                mGameObject.transform.Rotate(Vector3.Cross(mGameObject.transform.forward, mGameObject.transform.right), 1.0f);
+                Debug.Log("collider name: " + hitInfoArray[i].collider.gameObject.name);
+                if ((hitInfoArray[i].collider.gameObject.GetInstanceID() != mGameObject.collider.gameObject.GetInstanceID()))
+                {
+                    hitInfo = hitInfoArray[i];
+                    hitCollider = hitInfoArray[i].collider;
+
+                    if (mPreviousCollisionState == CollisionState.NoCollision)
+                    {
+                        mCurrentCollisionState = CollisionState.NewCollision;
+                    }
+                    else if (mPreviousCollisionState == CollisionState.NewCollision || mPreviousCollisionState == CollisionState.CurrentlyColliding)
+                    {
+                        mCurrentCollisionState = CollisionState.CurrentlyColliding;
+                    }
+                    break;
+                }
             }
         }
-
-        //float closestDistance = float.MaxValue;
-        //Collider closestCollider = null;
-        //foreach (KeyValuePair<int, Collider> collider in mEnemyComponent.Triggers)
-        //{
-        //    Debug.Log("collision inst id: " + collider.Value.gameObject.GetInstanceID());
-        //    float distanceToCollision = Vector3.Distance(collider.Value.ClosestPointOnBounds(), mGameObject.transform.position);
-        //    if (closestCollider == null || closestDistance > )
-        //    {
-
-        //        closestCollider = collider.Value;
-        //    }
-        //}
-        //}
-        //otherwise meander around the scene
-        else
+        if (hitCollider == null)
         {
+            mCurrentCollisionState = CollisionState.NoCollision;
+        }
+
+        //process collisions
+        if (mCurrentCollisionState == CollisionState.NewCollision)
+        {
+            //Debug.Log("new collision: " + hitCollider.gameObject.name);
+
+            mInitialDirection = mGameObject.transform.forward;
+            mInverseDirection = Vector3.Cross(mGameObject.transform.forward, Vector3.up);
+
+            mLateralForceDirection = hitInfo.normal;
+            //Debug.LogError("current velocity: " + mGameObject.rigidbody.velocity);
+            //Debug.Break();
+        }
+        else if (mCurrentCollisionState == CollisionState.CurrentlyColliding)
+        {
+            mMoveVector = mLateralForceDirection.normalized * mMovementSpeed;
+
+            //Debug.Log("still colliding with: " + hitCollider.gameObject.name +
+            //            " lerping - from: " + mInitialDirection +
+            //            " to: " + mLateralForceDirection +
+            //            " curr: " + mMoveVector);
+
+            Debug.DrawRay(mGameObject.transform.position,
+                          mMoveVector * 100.0f,
+                          Color.green);
+            Debug.DrawRay(hitInfo.point,
+                            hitInfo.normal* 100.0f,
+                            Color.red);
+        }
+        else if(mCurrentCollisionState == CollisionState.NoCollision)
+        {
+            //Debug.Log("no collisions");
+            
             double elapsedSeconds = (DateTime.Now - mPickedNewWanderDirectionTime).TotalSeconds;
             if (elapsedSeconds >= mWanderInDirectionSeconds)
             {
                 mUpdateRandomDirection = true;
-                mWanderInDirectionSeconds = UnityEngine.Random.Range(1.0f, 4.0f);
+                mWanderInDirectionSeconds = UnityEngine.Random.Range(2.0f, 4.0f);
                 mPickedNewWanderDirectionTime = DateTime.Now;
             }
 
             if (mUpdateRandomDirection || mRandomRotation == Vector3.zero)
             {
-                mRandomRotation = new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f),
-                                                UnityEngine.Random.Range(-1.0f, 1.0f),
-                                                UnityEngine.Random.Range(-1.0f, 1.0f));
+                mRandomRotation = new Vector3(  UnityEngine.Random.Range(-0.25f, 0.25f),
+                                                UnityEngine.Random.Range(-0.25f, 0.25f),
+                                                UnityEngine.Random.Range(-0.25f, 0.25f));
                 mUpdateRandomDirection = false;
 
-                Debug.Log("new random rotation: " + mRandomRotation);
+                //Debug.Log("new random rotation: " + mRandomRotation);
+                mMoveVector = mRandomRotation.normalized * mMovementSpeed;
             }
-
-            mGameObject.transform.Rotate(mRandomRotation.x, mRandomRotation.y, mRandomRotation.z);
         }
 
-        moveVector = mGameObject.transform.forward.normalized * mMovementSpeed;
-        mCharacterController.transform.forward = mGameObject.transform.forward;
-        mCharacterController.Move(moveVector);
+        mGameObject.GetComponent<CharacterController>().Move(mMoveVector);
+                mGameObject.rigidbody.rotation = Quaternion.Lerp(Quaternion.LookRotation(mGameObject.transform.forward.normalized),
+                                                         Quaternion.LookRotation(mMoveVector.normalized),
+                                                         Time.deltaTime * mTurnSpeed);
+
+        mPreviousCollisionState = mCurrentCollisionState;
     }
 }
 
