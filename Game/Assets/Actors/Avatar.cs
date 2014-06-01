43637b271348fa07e6dd26f921ca338906d719d6
zwetol <l.zwetol@gmail.com>
2014-06-01 17:04:32 -0400

    -commiting before merge

--- Game/Assets/Actors/Avatar.cs
+++ Game/Assets/Actors/Avatar.cs
@@ -27,7 +27,7 @@ public class Avatar : ActorBase
 		backgroundAudio.clip = Resources.Load ("Sounds/ANW1043_01_Creeping-Shadow") as AudioClip;
 		backgroundAudio.loop = true;
 		backgroundAudio.playOnAwake = true;
-		backgroundAudio.volume = 0.02f;
+		backgroundAudio.volume = 0.03f;
 		cameraGO.audio.Play ();
 
 		mGameObject.AddComponent<Rigidbody> ();
@@ -35,20 +35,27 @@ public class Avatar : ActorBase
 	
 		var audioSource = mGameObject.AddComponent<AudioSource>();
 		audioSource.clip = Resources.Load ("Sounds/211389__monica137142__underwater---played when character moves") as AudioClip;
+		audioSource.volume = 1f;
     }
 
     protected override void ActorUpdate()
     {
+		bool pressedMovementButton = false;
+
 		//on "A" key, add force up
 		if (Input.GetKeyDown(KeyCode.JoystickButton0))
 		{
 			mGameObject.rigidbody.AddForce(Vector3.up * 200);
+
+			pressedMovementButton = true;
 		}
 		//on "X" key, add force up and forward
 		if (Input.GetKeyDown(KeyCode.JoystickButton2))
 		{
 			mGameObject.rigidbody.AddForce(Vector3.up * 100);
 			mGameObject.rigidbody.AddForce(mGameObject.transform.forward * 100);
+
+			pressedMovementButton = true;
 		}
 		
 
@@ -69,14 +76,29 @@ public class Avatar : ActorBase
 		}
 
 		//adding sound for when user inputs movement (not including velocity)
-		if (horizontalMovement != 0 || verticalMovement != 0 || horizontalRotation != 0) {
+		if (horizontalMovement != 0 || verticalMovement != 0 || horizontalRotation != 0 || pressedMovementButton == true) {
 			if(!mGameObject.audio.isPlaying){
 				mGameObject.audio.Play();
 			}
 		}
 		else{
+		//	fadeOut();
 			mGameObject.audio.Stop();
-		}
 
-    }
+			//for (var i = 9; i > 0; i--){
+			//	mGameObject.audio.volume = i * .1;
+			
+			}
+			
+		}
+	//function fadeOut() {
+	//	if(audioSource.volume > 0.1)
+	//	{
+	//		audioSource.volume -= 0.1 * Time.deltaTime;
+
 }
+
+
+		
+		
+		
