cde4eeb76830c23d12b0dcb91f2e38db6c0f6e4a
Matt Sable <duke.sable@gmail.com>
2015-01-24 17:31:42 -0500

    All important buttons on controller mapped

---
+++ hinder/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ControllerManager : MonoBehaviour{
+
+    public event Action<ButtonLabel, PlayerNumber> OnButtonPress;
+    public event Action<AnalogLabel, PlayerNumber, float> OnAnalogMovement;
+
+    public enum AnalogLabel
+    {
+        LeftAnalogX,
+        LeftAnalogY,
+        RightAnalogX,
+        RightAnalogY
+    }
+
+    public enum ButtonLabel
+    {
+        TopButton,
+        LeftButton,
+        BottomButton,
+        RightButton,
+        L1Button,
+        L2Button,
+        R1Button,
+        R2Button,
+        StartButton
+    }
+
+    public enum PlayerNumber
+    {
+        P1,
+        P2
+    }
+
+    public float _analogTolerance = 0.2f;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        foreach (string button in Enum.GetNames(typeof(ButtonLabel)))
+        {
+            foreach (string playerNum in Enum.GetNames(typeof(PlayerNumber)))
+            {
+                if (Input.GetButtonDown(button + playerNum))
+                {
+                    OnButtonPress((ButtonLabel)Enum.Parse(typeof(ButtonLabel), button), (PlayerNumber) Enum.Parse(typeof(PlayerNumber), playerNum));
+                }
+            }
+        }
+
+        foreach (string analog in Enum.GetNames(typeof(AnalogLabel)))
+        {
+            foreach (string playerNum in Enum.GetNames(typeof(PlayerNumber)))
+            {
+                float analogMovement = Input.GetAxis(analog + playerNum);
+                if (Math.Abs(analogMovement) > _analogTolerance)
+                {
+                    OnAnalogMovement((AnalogLabel)Enum.Parse(typeof(AnalogLabel), analog), (PlayerNumber)Enum.Parse(typeof(PlayerNumber), playerNum), analogMovement);
+                }
+            }
+        }
+	}
+}
