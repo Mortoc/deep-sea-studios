3ba059b3a30e5727353c16097d133707e70d635d
Mortoc <mortoc@gmail.com>
2015-01-24 17:11:46 -0500

    Created the state machine for the prototype character

---
+++ hinder/Assets/Player/Being.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Being : MonoBehaviour 
+{
+
+	private bool _isGrounded;
+
+	public void Move(float direction, float power)
+	{
+	}
+}
---
+++ hinder/Assets/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	[SerializeField]
+	private float _speed = 10.0f;
+
+	void Awake()
+	{
+	}
+
+	void FixedUpdate()
+	{
+		var movement = Vector3.zero;
+
+		if( Input.GetKey(KeyCode.A) )
+		{
+			movement += Vector3.left;
+		}
+		
+		if( Input.GetKey(KeyCode.D) )
+		{
+			movement += Vector3.right;
+		}
+
+		//_controller.Move (movement * _speed * Time.fixedDeltaTime);
+	}
+}
