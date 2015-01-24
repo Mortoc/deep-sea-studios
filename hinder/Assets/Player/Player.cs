2a1d5fee2ed35f7fd7c97abe4f54ea959718c192
Mortoc <mortoc@gmail.com>
2015-01-24 17:58:42 -0500

    Player sorta works now

--- hinder/Assets/Player/Player.cs
+++ hinder/Assets/Player/Player.cs
@@ -1,29 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour 
 {
 	[SerializeField]
 	private float _speed = 10.0f;
+	
+	[SerializeField]
+	private float _jumpForce = 100.0f;
+
+	[SerializeField]
+	private CircleCollider2D _footCollider;
+
+	private Animator _animator;
+	private Vector3 _rightScale;
+	private Vector3 _leftScale;
+
+	private bool _grounded = true;
+
+	[SerializeField]
+	private LayerMask _groundLayers;
 
 	void Awake()
 	{
+		_animator = GetComponent<Animator>();
+		_rightScale = transform.localScale;
+		_leftScale = _rightScale;
+		_leftScale.x *= -1.0f;
+	}
+
+	void Update()
+	{
+		if( _grounded && Input.GetKeyDown (KeyCode.Space) )
+		{
+			_grounded = false;
+			rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+		}
 	}
 
 	void FixedUpdate()
 	{
-		var movement = Vector3.zero;
+		var movement = Input.GetAxis ("Horizontal");
+		var speed = movement * _speed * Time.fixedDeltaTime;
+		_animator.SetFloat ("X-Speed", speed);
 
-		if( Input.GetKey(KeyCode.A) )
+		rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+
+		if( speed < 0.0f ) 
 		{
-			movement += Vector3.left;
+			transform.localScale = _leftScale;
 		}
-		
-		if( Input.GetKey(KeyCode.D) )
+		else
 		{
-			movement += Vector3.right;
+			transform.localScale = _rightScale;
 		}
 
-		//_controller.Move (movement * _speed * Time.fixedDeltaTime);
+		_grounded = Physics2D.Raycast(
+			rigidbody2D.position + _footCollider.center, 
+			Vector2.up * -1.0f, 
+			_footCollider.radius * 1.5f, 
+			_groundLayers.value
+		);
+
+		_animator.SetBool("IsGrounded", _grounded);
 	}
 }
