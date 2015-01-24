cf654e0d84fb5095e5e94fefa51d63a6e6d651c3
Mortoc <mortoc@gmail.com>
2015-01-24 13:51:00 -0500

    created the UI folder:

---
+++ hinder/Assets/UI/HinderancePickerUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HinderancePickerUI : MonoBehaviour 
+{
+	public void SelectedHinderance(string hinderance)
+	{
+		Debug.Log("Selected Hinderance: " + hinderance);
+	}
+}
