99d7b710d2d28e026e52d9d15d140b8c5f6fe98f
Matt Sable <duke.sable@gmail.com>
2015-01-24 19:05:48 -0500

    Merging

--- hinder/Assets/Player/Minion.cs
+++ hinder/Assets/Player/Minion.cs
@@ -3,5 +3,20 @@ using System.Collections;
 
 public class Minion : Being 
 {
+    private float _hitPoints = 10.0f;
+    private float _damage = 0.0f;
 
+    void Update()
+    {
+        rigidbody2D.AddForce(Vector2.right * 3);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _damage += damage;
+        if (_damage >= _hitPoints)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
 }
---
+++ hinder/Assets/Player/MinionSpawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionSpawn : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject _minionPrefab;
+
+    public int _numMinions = 3;
+
+	void Start () 
+    {
+        SpawnMinion(_numMinions);
+	}
+
+    void SpawnMinion(int num)
+    {
+        for (int i = 0; i < num; i++)
+        {
+            var minion = (GameObject)Instantiate(_minionPrefab);
+            minion.transform.position = transform.position;
+        }
+    }
+	
+}
