aa80fb3ec972f1b7c53fad2d0e7a2ebad848be59
Mortoc <mortoc@gmail.com>
2015-01-30 21:30:17 -0500

    character is mayaified and sorted in unity

---
+++ hinder/Assets/Editor/PlanesToSprites.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public static class PlanesToSprites
+{
+	private static Regex _sortingLayerRegex = new Regex("sort([0-9])");
+
+	[MenuItem("Tools/Assign sorting order")]
+	public static void ConvertPlanesToSprites()
+	{
+		if( Selection.activeGameObject ) 
+		{
+			foreach(var meshRenderer in Selection.activeGameObject.GetComponentsInChildren<MeshRenderer>())
+			{
+				var match = _sortingLayerRegex.Match(meshRenderer.gameObject.name);
+				int sortingOrder;
+				if( int.TryParse(match.Groups[1].Value, out sortingOrder) )
+				{
+					meshRenderer.sortingOrder = sortingOrder;
+				}
+			}
+		}
+	}
+}
