2a659236cf417cf44a12292f5dbe7d1d7f626aa6
Mortoc <mortoc@gmail.com>
2015-02-11 18:20:21 -0500

    this should actually fix the ios build now

---
+++ level-edit/Assets/MiscScripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	[SerializeField]
+	private Vector3 _spinAxes = Vector3.up * 10.0f;
+
+	void Update() {
+		transform.Rotate(_spinAxes * Time.deltaTime);
+	}
+}
