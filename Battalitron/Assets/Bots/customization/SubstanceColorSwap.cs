6c5fd63804d2039ac6b527b05877f44d4acacbc7
Mortoc <mortoc@gmail.com>
2015-03-04 17:25:52 -0500

    built a test for a dyemap pipeline

---
+++ Battalitron/Assets/Bots/customization/SubstanceColorSwap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubstanceColorSwap : MonoBehaviour 
+{
+	[SerializeField]
+	private ProceduralMaterial _substance;
+
+	[SerializeField]
+	private string _colorName;
+
+	public void SetColorRed()
+	{
+		SetColor(Color.red);
+	}
+	
+	public void SetColorGreen()
+	{
+		SetColor(Color.green);
+	}
+	
+	public void SetColorBlue()
+	{
+		SetColor(Color.blue);
+	}
+
+	public void SetColor(Color c)
+	{
+		_substance.SetProceduralColor(_colorName, c);
+		_substance.RebuildTextures();
+	}
+}
