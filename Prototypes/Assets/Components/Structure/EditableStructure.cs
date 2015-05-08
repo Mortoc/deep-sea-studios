25ac7e5c6dc08e38f46a615286c0b1409fe43180
Mortoc <Mortoc@gmail.com>
2015-05-07 22:59:05 -0400

    created a simple UI for adding parts to the scene

---
+++ Prototypes/Assets/Components/Structure/EditableStructure.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rand = UnityEngine.Random;
+
+namespace DSS
+{
+    public class EditableStructure : MonoBehaviour
+    {
+        public enum Connectivity
+        {
+            None = 0,
+            All = 0xFFFF,
+            Up = 1,
+            Down = 2,
+            Left = 4,
+            Right = 8,
+            Forward = 16,
+            Backward = 32
+        }
+
+        private int _connections;
+    }
+}
