e9fa5b3f4a7fef9d7dc76cbcf93a155cd20682ad
Mortoc <Mortoc@gmail.com>
2015-05-07 20:44:47 -0400

    Linear Actuator is ready to go

---
+++ Prototypes/Assets/Components/LinearActuator/LinearActuator.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rand = UnityEngine.Random;
+
+namespace DSS
+{
+    public class LinearActuator : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody _end1;
+
+        [SerializeField]
+        private Rigidbody _end2;
+
+        [SerializeField]
+        private Rigidbody _center;
+
+        [SerializeField]
+        private ConstantForce _expandForce;
+
+        [SerializeField]
+        private float _expandPower = 100.0f;
+
+        [SerializeField]
+        private Plug _plug;
+
+        private void IgnoreAllCollisions(IList<Collider> colliders)
+        {
+            for (var i = 0; i < colliders.Count; ++i)
+            {
+                for (var j = i + 1; j < colliders.Count; ++j)
+                {
+                    Physics.IgnoreCollision(colliders[i], colliders[j]);
+                }
+            }
+        }
+
+        void Awake()
+        {
+            IgnoreAllCollisions(GetComponentsInChildren<Collider>());
+        }
+
+        void OnEnable()
+        {
+            _plug.OnPower += Expand;
+            _plug.OnPowerLoss += Contract;
+        }
+
+        void OnDisable()
+        {
+            _plug.OnPower -= Expand;
+            _plug.OnPowerLoss -= Contract;
+        }
+
+        private void Expand()
+        {
+            _expandForce.relativeForce = Vector3.left * _expandPower;
+        }
+
+        private void Contract()
+        {
+
+            _expandForce.relativeForce = Vector3.left * -_expandPower;
+        }
+    }
+}
