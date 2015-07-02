2202c2e31174b0f822a543cdaa08f190d36a5698
Mortoc <Mortoc@gmail.com>
2015-07-01 20:47:10 -0400

    rescaled the scene to me sane for physics

--- Prototypes/Assets/Shared Assets/Scripts/Camera/SceneCamera.cs
+++ Prototypes/Assets/Shared Assets/Scripts/Camera/SceneCamera.cs
@@ -3,7 +3,19 @@ using System.Collections;
 
 namespace DSS
 {
-    public class SceneCamera : MonoBehaviour
-    {
-    }
+	[RequireComponent(typeof(Camera))]
+	public class SceneCamera : MonoBehaviour
+	{
+		[SerializeField]
+		[Range(1, 8)]
+		private int _downsample = 1;
+		
+		void Start()
+		{
+			var cam = GetComponent<Camera> ();
+			
+			cam.targetTexture.width = Screen.width / _downsample;
+			cam.targetTexture.height = Screen.height / _downsample;
+		}
+	}
 }
